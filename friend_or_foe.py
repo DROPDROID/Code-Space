6774c8bff7ffda9a7eeb3010ba38c8d9dd184d67
David Chubrykov <davidchubrykov@MAC-B2E61C.station>
2024-09-06 12:15:52 +0100

    add second solution

--- friend_or_foe.py
+++ friend_or_foe.py
@@ -7,3 +7,10 @@ def friend(x):
 
 
 friend({"Ryan", "Kieran", "Jason", "Yous"})
+
+
+def friend(x):
+    res = [i for i in x if len(i) == 4]
+    print(res)
+
+friend({"Ryan", "Kieran", "Jason", "Yous"})
