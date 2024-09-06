bc6f6a3ef48b3f4685dc312e5bb4b6e9736e4759
DROPDROID <57584979+DROPDROID@users.noreply.github.com>
2024-09-06 12:49:18 +0100

    Sum of numbers 3 (#2)
    * Start working
    
    * test new line
    
    * added new file, sum of numbers_3
    
    ---------
    
    Co-authored-by: David Chubrykov <davidchubrykov@MAC-B2E61C.station>

--- sum_of_numbers_3.py
+++ sum_of_numbers_3.py
@@ -1,1 +1,10 @@
-sdfdsf
+def get_sum(a,b):
+    if a == b:
+        return a
+    elif a < b:
+        return sum(list(range(a, b+1)))
+    elif a > b:
+        return sum(list(range(b, a+1)))
+
+
+get_sum(-2, -1)
