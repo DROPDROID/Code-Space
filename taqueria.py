e6dd4a34fa0074698541d1bc3f600df3dd93662d
David Chubrykov <57584979+DROPDROID@users.noreply.github.com>
2024-09-13 20:13:06 +0100

    added a new cs50 file taqueria (#20)

---
+++ taqueria.py
@@ -0,0 +1,28 @@
+def order():
+
+    menu = {
+        "Baja Taco": 4.25,
+        "Burrito": 7.50,
+        "Bowl": 8.50,
+        "Nachos": 11.00,
+        "Quesadilla": 8.50,
+        "Super Burrito": 8.50,
+        "Super Quesadilla": 9.50,
+        "Taco": 3.00,
+        "Tortilla Salad": 8.00,
+    }
+
+    cost = 0
+
+    while True:
+        try:
+            item = input("Item: ").title()
+            cost += menu[item]
+            print(f"${cost:.2f}")
+        except EOFError:
+            print("\n")
+            break
+        except KeyError:
+            print("Dish doesn't exist")
+
+order()
